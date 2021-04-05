4c551b1df33dbe47166efc311eb5112e76eacdc1
Dryuss <gipto93@gmail.com>
2021-04-05 16:37:13 +0400

    fix security configuration

--- src/main/java/com/server/config/SecurityConfig.java
+++ src/main/java/com/server/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.server.config;
 
-import com.server.model.services.AuthProvider;
 import com.server.model.services.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -29,12 +26,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/",
-                        "/login**", "/registration", "/dealership/cars").permitAll()
+
+                .antMatchers("/resources/**", "/login**",
+                        "/registration", "/news", "/cars").permitAll()
+
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login")
-                .defaultSuccessUrl("/").failureUrl("/login?error").permitAll()
-                .and().logout().logoutSuccessUrl("/").permitAll();
+
+                .failureUrl("/login?error").permitAll()
+                .and().logout().permitAll();
     }
 
     @Override
